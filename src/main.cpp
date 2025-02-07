4765a3b14343bacf9b64ebf25bef8f2d38d93653
Nhat Tran <tranleminhnhat0902@gmail.com>
2025-02-08 02:50:09 +0700

    feat: Change ladybrown speed to 50% of max speed

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ const double CONVEYOR_TRANSPORT_SPEED_PERCENT = 75;
 
 /// @brief The speed of the ladybrown as a percentage of its max speed (200
 /// rpm).
-const double LADYBROWN_SPEED_PERCENT = 75;
+const double LADYBROWN_SPEED_PERCENT = 50;
 
 /// @brief Vision sensor signature ID for the red donut.
 const uint32_t RED_SIG_ID = 1;
