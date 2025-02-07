3599622a2cc751b70a0824119427fb57d61d1ae0
Nhat Tran <tranleminhnhat0902@gmail.com>
2025-02-07 11:38:10 +0700

    feat: Add Selector class for selecting programs for testing on the controller

---
+++ include/selector.hpp
@@ -0,0 +1,42 @@
+/// @file selector.hpp
+///
+/// @brief Contains the Selector class for selector on the controller.
+
+#include "main.h"
+
+/// @brief A list of values for each key of the selector.
+struct Key {
+  /// @brief The name of the key.
+  std::string name;
+  /// @brief The list of values for the key.
+  std::vector<std::string> values;
+  /// @brief The current index of the key. 
+  int index;
+};
+
+class Selector {
+  /// @brief The controller object.
+  pros::Controller controller;
+
+  /// @brief The list of keys for the selector.
+  std::vector<Key> keys;
+
+  /// @brief The index of the current selected key.
+  int key_index;
+
+ public:
+  /// @brief Constructor for the Selector class.
+  /// @param controller The controller object to use for the selector.
+  /// @param keys The list of keys for the selector.
+  Selector(pros::Controller controller, std::initializer_list<Key>& keys);
+
+  /// @brief Returns the current selected key.
+  /// @return The current selected key.
+  Key get_current_key();
+
+  /// @brief Updates the selector and returns the current selected key.
+  void update();
+
+  /// @brief Displays the selector on the controller.
+  void display();
+};
---
+++ src/selector.cpp
@@ -0,0 +1,40 @@
+#include "selector.hpp"
+
+Selector::Selector(pros::Controller controller,
+                   std::initializer_list<Key>& keys)
+    : controller(controller), keys(keys), key_index(0) {}
+
+Key Selector::get_current_key() { return keys[key_index]; }
+
+void Selector::update() {
+  // Cycle through the keys with the controller left and right buttons.
+  if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
+    key_index--;
+  } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+    key_index++;
+  }
+
+  // Wrap around the keys.
+  key_index = (key_index + keys.size()) % keys.size();
+
+  Key current_key = get_current_key();
+
+  // Cycle through the values with the controller up and down buttons.
+  if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
+    current_key.index++;
+  } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+    current_key.index--;
+  }
+
+  // Wrap around the values.
+  current_key.index = (current_key.index + current_key.values.size()) %
+                      current_key.values.size();
+}
+
+void Selector::display() {
+  Key current_key = get_current_key();
+
+  // Display the current key on the controller.
+  controller.print(0, 0, "%s: %s", current_key.name.c_str(),
+                   current_key.values[current_key.index].c_str());
+}
